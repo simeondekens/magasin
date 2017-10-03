e19e75294d1c3c5f7a1ad470c6cd9ca4a102cadb
simeon.dekens <simeon-dekens@hotmail.fr>
2017-10-03 15:29:21 +0200

    Commit menu

--- lib/php/menu.php
+++ lib/php/menu.php
@@ -4,4 +4,7 @@
     <li><a><?php print "Option1" ?></a></li>
     <li><a><?php print "Option2" ?></a></li>
     <li><a><?php print "Option3" ?></a></li>
+    <li><a><?php print "Option4" ?></a></li>
+    <li><a><?php print "Option5" ?></a></li>
+    <li><a><?php print "Option6" ?></a></li>
 </ul>
